290e765928ba3ffd1d3eb4cefcf376d8c0992545
Kelrynn <cameroncastells@gmail.com>
2017-09-21 08:37:52 -0600

    building game logic

--- js/app.js
+++ js/app.js
@@ -1,41 +1,55 @@
 console.log("JS Loaded!");
 $(function() {
 
-resetBoard();
-let currentPlayer = 0;
-let hasMarked = false;
-let p1 = prompt("Enter name for Player 1");
-let p2 = prompt("Enter name for Player 2");
-$('#p1').html(p1);
-$('#p2').html(p2);
+    let currentPlayer = 0;
+    let hasMarked = false;
+    let p1 = prompt("Enter name for Player 1");
+    let p2 = prompt("Enter name for Player 2");
+    $('#p1').html(p1);
+    $('#p2').html(p2);
+    resetBoard();
 
-function resetBoard() {
-    $('td').removeClass('x o');
-
-}
-
-$('#reset').click(resetBoard);
-
-$('table').on('click', 'td', mark);
-$('#nextTurn').click(nextTurn);
-
-
-function mark() {
-    console.log($(this));
-    let mark = ["x", "o"];
-    if (!hasMarked) {
-        if (!($(this).hasClass('x') || $(this).hasClass('o'))) {
-            $(this).addClass(mark[currentPlayer]);
+    function resetBoard() {
+        $('td').removeClass('x o');
+        hasMarked = false;
+        $('#nextTurn').css('opacity', '.3');
+        if (currentPlayer) {
+        	currentPlayer = 0;
+        	$('.player').toggleClass('active');
         }
-        hasMarked = true;
     }
-}
 
-function nextTurn(){
-	$('.player').toggleClass('active');
-	currentPlayer = (currentPlayer + 1) % 2;
-	hasMarked = false;
-}
+    $('#reset').click(resetBoard);
+
+    $('table').on('click', 'td', mark);
+    $('#nextTurn').click(nextTurn);
+
+
+    function mark() {
+        console.log($(this));
+        let mark = ["x", "o"];
+        if (!hasMarked) {
+            if (!($(this).hasClass('x') || $(this).hasClass('o'))) {
+                $(this).addClass(mark[currentPlayer]);
+                hasMarked = true;
+                $('#nextTurn').css('opacity', '1');
+            }
+        }
+
+    }
+
+    function nextTurn() {
+        if (hasMarked) {
+            $('.player').toggleClass('active');
+            currentPlayer = (currentPlayer + 1) % 2;
+            hasMarked = false;
+            $('#nextTurn').css('opacity', '.3');
+        }
+    }
+
+
+
+
 
 
 
